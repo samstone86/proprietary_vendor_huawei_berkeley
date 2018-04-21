4a17b589972a59124ecffb621f120a4ec7696fb4
Philipp Vogel <philippwolf86.pw@gmail.com>
2018-04-21 19:00:34 +0200

    berkeley: fix makefile

--- berkeley-vendor.mk
+++ berkeley-vendor.mk
@@ -1,3 +1,18 @@
+# Copyright (C) 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_COPY_FILES += \
     vendor/huawei/berkeley/proprietary/etc/charger/1080x1920/battery_01.png:system/etc/charger/1080x1920/battery_01.png \
     vendor/huawei/berkeley/proprietary/etc/charger/1080x1920/battery_02.png:system/etc/charger/1080x1920/battery_02.png \
     vendor/huawei/berkeley/proprietary/etc/charger/1080x1920/battery_03.png:system/etc/charger/1080x1920/battery_03.png \
